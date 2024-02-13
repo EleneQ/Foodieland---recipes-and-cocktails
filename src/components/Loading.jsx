512c2352e5ae570ba4c8b17ea203eef9ece56e8d
Elene Qaldani <eleneqaldani1@gmail.com>
2024-02-13 13:22:01 +0400

    implemented a better loader

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,5 +1,9 @@
 const Loading = () => {
-  return <p className="padding-x max-width font-medium">Loading...</p>;
+  return (
+    <div className="mx-auto mt-10">
+      <div className="border-gray-300 h-10 w-10 animate-spin rounded-full border-4 border-t-accent-pink-500" />
+    </div>
+  );
 };
 
 export default Loading;
